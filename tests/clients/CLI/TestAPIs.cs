04d910aa801d5ced2a2380a2b56d641263dd5b0d
Agarwal <y508837@softwareag.com>
2020-02-19 10:25:00 +0530

    Updating with latest code

--- tests/clients/CLI/TestAPIs.cs
+++ tests/clients/CLI/TestAPIs.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using LibGit2Sharp;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.IO.Compression;
 
 namespace Repo.Clients.CLI.Commands.Tests
 {
@@ -13,10 +16,10 @@ namespace Repo.Clients.CLI.Commands.Tests
         public static readonly DirectoryInfo SrcDir = TestCurrentDir.Parent.Parent.Parent.Parent.Parent.Parent;
         public static readonly string UmoyaPath = SrcDir.FullName + Constants.PathSeperator + "src" + Constants.PathSeperator + "clients" + Constants.PathSeperator + "CLI" + Constants.PathSeperator + "bin" + Constants.PathSeperator + "release" + Constants.PathSeperator + "netcoreapp2.2" + Constants.PathSeperator + "publish" + Constants.PathSeperator + "umoya.dll";
 
-        public static readonly string RepoPath = SrcDir.FullName + Constants.PathSeperator + "src" + Constants.PathSeperator + "service-components" + Constants.PathSeperator + "Umoya" + Constants.PathSeperator + "bin" + Constants.PathSeperator + "Debug" + Constants.PathSeperator + "netcoreapp2.2" + Constants.PathSeperator + "publish";
+        public static readonly string RepoPath = SrcDir.FullName + Constants.PathSeperator + "src" + Constants.PathSeperator + "service-components" + Constants.PathSeperator + "Umoya" + Constants.PathSeperator + "bin" + Constants.PathSeperator + "Debug" + Constants.PathSeperator + "netcoreapp2.2";
 
         public static readonly string DotNetCommand = "dotnet";
-        public static readonly string ArgumentName = "umoya.dll";
+        public static readonly string UmoyaBinaryFileName = "umoya.dll";
         public static Process RepoProcess;
 
         public static readonly string TestDataGitHubRepoURL = "https://github.com/nimeshgit/umoya-testdata";
@@ -32,22 +35,33 @@ namespace Repo.Clients.CLI.Commands.Tests
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.WorkingDirectory = WorkingDir;
             process.Start();
-            if (!ActualoutputFilePath.Equals(string.Empty)) File.WriteAllText(ActualoutputFilePath, process.StandardOutput.ReadToEnd());
             process.WaitForExit();
+            if (!ActualoutputFilePath.Equals(string.Empty)) File.WriteAllText(ActualoutputFilePath, process.StandardOutput.ReadToEnd());
+
         }
 
         public static bool CompareActualAndExpectedOutput(string TestName, string TestScenariosName, out string DiffOutput)
         {
             bool Status = true;
             DiffOutput = "Diff";
-            List<string> ExpectedContents = new List<string>().ToList();
-            List<string> ActualContents = new List<string>();
+
+            string ExpectedContents = string.Empty;
+            string ActualContents = string.Empty;
+            
             string ExpectedOutputFilePath = Constants.DefaultTestDataDir + Constants.PathSeperator + "expected-output" + Constants.PathSeperator + TestName + Constants.PathSeperator + TestScenariosName + ".txt";
-            ExpectedContents = File.ReadAllLines(ExpectedOutputFilePath).ToList();
+            ExpectedContents = string.Join( ",", File.ReadAllLines(ExpectedOutputFilePath).ToList())
+            .Replace('\n', ' ').Replace('\t', ' ').Replace('\r', ' ').Replace("  ", string.Empty).Replace("   ", string.Empty)
+            .Replace(" ", string.Empty);
+            
             string ActualOutputFilePath = Constants.DefaultTestDataDir + Constants.PathSeperator + "actual-output" + Constants.PathSeperator + TestName + Constants.PathSeperator + TestScenariosName + ".txt";
-            ActualContents = File.ReadAllLines(ActualOutputFilePath).ToList();
-            var DifferenceBetweenExpectedAndActualOutput = ActualContents.Except(ExpectedContents).ToArray();
-            if (DifferenceBetweenExpectedAndActualOutput.Length > 0)
+             ActualContents = string.Join( ",", File.ReadAllLines(ActualOutputFilePath).ToList())
+            .Replace('\n', ' ').Replace('\t', ' ').Replace('\r', ' ').Replace("  ", string.Empty).Replace("   ", string.Empty)
+            .Replace(" ", string.Empty);
+
+            var DifferenceBetweenExpectedAndActualOutput = string.Compare(ActualContents,ExpectedContents);
+            System.Console.WriteLine("ActualContents " + ActualContents);
+            System.Console.WriteLine("ExpectedContents "+ ExpectedContents);
+            if (DifferenceBetweenExpectedAndActualOutput > 0)
             {
                 Status = false;
                 DiffOutput = string.Join(',', DifferenceBetweenExpectedAndActualOutput);
@@ -71,20 +85,25 @@ namespace Repo.Clients.CLI.Commands.Tests
         }
         public static bool InitializeTestDataSetup()
         {
-            if (!IsInitialized)
+            if (IsInitialized) return true;
+            try
             {
+                DirectoryInfo parentDirectoryInfo = new DirectoryInfo(Constants.DefaultTestDataDir);
                 if (Directory.Exists(Constants.DefaultTestDataDir))
                 {
-                    DirectoryInfo parentDirectoryInfo = new DirectoryInfo(Constants.DefaultTestDataDir);
                     ClearReadOnly(parentDirectoryInfo);
                     Directory.Delete(Constants.DefaultTestDataDir, true);
                 }
                 Directory.CreateDirectory(Constants.DefaultTestDataDir);
-                Repository.Clone(TestDataGitHubRepoURL, Constants.DefaultTestDataDir);
-
+                CloneGitRepository(Environment.CurrentDirectory, TestDataGitHubRepoURL);
                 string ZMODPath = Constants.DefaultTestDataDir + Constants.PathSeperator + "temp";
                 Directory.CreateDirectory(ZMODPath);
                 IsInitialized = true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Do(ex.StackTrace);
+                IsInitialized = false;
             }
             return IsInitialized;
         }
@@ -93,40 +112,94 @@ namespace Repo.Clients.CLI.Commands.Tests
         {
             bool Status = true;
             CaptureConsoleOutPut("init", string.Empty, ZMODPath, string.Empty);
+            CaptureConsoleOutPut("info", "-sp False", ZMODPath, string.Empty);
             return Status;
         }
 
         public static bool IsRepoRunning()
         {
-            //ZMOD is configured under ZMODPath
-            bool Status = false;            //
-            if (RepoProcess != null)
+            bool Status = false;
+            int StartingPort = 8007;
+            try
             {
-                if (!RepoProcess.HasExited) Status = true;
+                IPEndPoint[] endPoints;
+                List<int> PortArray = new List<int>();
+                IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+                // getting active connections 
+                TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+                PortArray.AddRange(from n in connections
+                                   where n.LocalEndPoint.Port == StartingPort
+                                   select n.LocalEndPoint.Port);
+
+                // getting active tcp listeneres - wcf service Listening in tcp
+                endPoints = properties.GetActiveTcpListeners();
+                PortArray.AddRange(from n in endPoints
+                                   where n.Port == StartingPort
+                                   select n.Port);
+                Status = (PortArray.Count > 0);
+            }
+            catch (SocketException ex)
+            {
+                System.Console.WriteLine(ex.StackTrace);
             }
             return Status;
+        }
+
+
+        public static bool CloneGitRepository(string DirectoryPathToClone, string GitRepositoryPath)
+        {
+            try
+            {
+                System.Console.WriteLine("DirectoryPathToClone: " + DirectoryPathToClone);
+                System.Console.WriteLine("GitRepositoryPath: " + GitRepositoryPath);
+                Process TempProcess = new Process();
+                TempProcess.StartInfo.FileName = "git";
+                TempProcess.StartInfo.Arguments = "clone " + GitRepositoryPath;
+                TempProcess.StartInfo.UseShellExecute = false;
+                TempProcess.StartInfo.CreateNoWindow = true;
+                TempProcess.StartInfo.WorkingDirectory = DirectoryPathToClone;
+                TempProcess.Start();
+                TempProcess.WaitForExit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Do("Error while starting repo: " + ex.StackTrace);
+                return false;
+            }
         }
 
         public static bool StartRepo()
         {
             //Start Repo 
+            bool Status = true;
             try
             {
-                System.Console.WriteLine("SurbhiRepoPath "+RepoPath);
-                RepoProcess = new Process();
-                RepoProcess.StartInfo.FileName = DotNetCommand;
-                RepoProcess.StartInfo.Arguments = ArgumentName;
-                RepoProcess.StartInfo.UseShellExecute = false;
-                RepoProcess.StartInfo.CreateNoWindow = true;
-                RepoProcess.StartInfo.WorkingDirectory = RepoPath;
-                RepoProcess.Start();
-                System.Console.WriteLine("Process started");
-                return true;
+                System.Console.WriteLine(RepoPath + Constants.PathSeperator + "publish" + Constants.PathSeperator + UmoyaBinaryFileName);
+                if (File.Exists(RepoPath + Constants.PathSeperator + "publish" + Constants.PathSeperator + UmoyaBinaryFileName))
+                {
+                    System.Console.WriteLine("Starting Repo from " + RepoPath + Constants.PathSeperator + "publish" + Constants.PathSeperator + UmoyaBinaryFileName + "  is found.");
+                    RepoProcess = new Process();
+                    RepoProcess.StartInfo.FileName = DotNetCommand;
+                    RepoProcess.StartInfo.Arguments = UmoyaBinaryFileName;
+                    RepoProcess.StartInfo.UseShellExecute = false;
+                    RepoProcess.StartInfo.CreateNoWindow = true;
+                    RepoProcess.StartInfo.WorkingDirectory = RepoPath + Constants.PathSeperator + "publish";
+                    RepoProcess.Start();
+                }
+                else
+                {
+                    System.Console.WriteLine("UmoyaBinaryFile " + RepoPath + Constants.PathSeperator + UmoyaBinaryFileName + " not found.");
+                    Status = false;
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return false;
+                RepoProcess.Kill();
+                System.Console.Error.WriteLine("Not able to start repo " + ex.StackTrace);
+                Status = false;
             }
+            return Status;
         }
         public static bool StopRepo()
         {
@@ -139,19 +212,79 @@ namespace Repo.Clients.CLI.Commands.Tests
         public static bool SetUpRepoServer()
         {
             bool Status = true;
-            string SourcePath = Constants.DefaultTestDataDir + Constants.PathSeperator + "repo-resources";
-            string DestinationPath = RepoPath;
-            //Now Create all of the directories
-            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
-                SearchOption.AllDirectories))
-                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+            try
+            {
+                string SourcePath = Constants.DefaultTestDataDir + Constants.PathSeperator + "repo-resources";
+                string DestinationPath = RepoPath + Constants.PathSeperator + "publish";
+                //Now Create all of the directories
+                System.Console.WriteLine("SetUpRepoServer " + SourcePath + "  " + DestinationPath);
+                foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+                    SearchOption.AllDirectories))
+                {
+                    System.Console.WriteLine("dirPath: " + dirPath);
+                    string TempDirectory = dirPath.Replace(SourcePath, DestinationPath);
+                    System.Console.WriteLine("Creating Directory (TempDirectory)" + TempDirectory);
+                    if (Directory.Exists(TempDirectory))
+                        Directory.Delete(TempDirectory, true);
+                    Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+                    System.Console.WriteLine(dirPath.Replace(SourcePath, DestinationPath));
+                }
+                //Copy all the files & Replaces any files with the same name
+                foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                    SearchOption.AllDirectories))
+                {
+                    System.Console.WriteLine("newPath: " + newPath);
+                    System.Console.WriteLine("Replaced newPath: " + newPath.Replace(SourcePath, DestinationPath));
+                    File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Do(ex.StackTrace);
+                System.Console.WriteLine("error " + ex.StackTrace);
+            }
+            return Status;
+        }
 
-            //Copy all the files & Replaces any files with the same name
-            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
-                SearchOption.AllDirectories))
-                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+        public static bool UnZipPublishFolder()
+        {
+            bool Status = true;
+            string startPath = TestAPIs.RepoPath;
+            string zipPath = "publish.zip";
+            string extractPath = startPath + Constants.PathSeperator + "publish";
+            var unzip = new Unzip(startPath + Constants.PathSeperator + zipPath);
+            try
+            {
+                System.Console.WriteLine(" Zip folder path: " + startPath + Constants.PathSeperator + zipPath);
+                if (File.Exists(startPath + Constants.PathSeperator + zipPath))
+                {
+                    if (Directory.Exists(extractPath))
+                    {
+                        Directory.Delete(extractPath, true);
+                    }
+                    ZipFile.ExtractToDirectory(startPath + Constants.PathSeperator + zipPath, extractPath);
+                    System.Console.WriteLine("Extracted folder to " + extractPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                unzip.Dispose();
+                Logger.Do(ex.StackTrace);
+            }
 
             return Status;
+
+        }
+        private static void ListFiles(Unzip unzip)
+        {
+            var tab = unzip.Entries.Any(e => e.IsDirectory) ? "\t" : string.Empty;
+            foreach (var entry in unzip.Entries.OrderBy(e => e.Name))
+            {
+                if (entry.IsFile)
+                {
+                    continue;
+                }
+            }
         }
     }
 }
